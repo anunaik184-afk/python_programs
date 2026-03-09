9f7c2f9fd715e301b28502cfbebb6e1947402cd6
Anu Narayana Naik <anunaik184@gmail.com>
2026-03-09 21:49:18 +0530

    Add greet and sum functions in function.py

---
+++ function .py
@@ -0,0 +1,18 @@
+#  1.Parameterized Greet Function:
+
+def greet(name="Student"):
+    print(f"Hello, {name}! Welcome to the Python course.")
+
+greet()  # Uses default value "Student"
+greet("Anu")  # Uses passed value "Anu"
+
+#  2.Greet Function:
+def greet():
+    print("Hello! Good Morning")
+greet()
+
+#  3.Sum Function:
+def add(a,b):
+    return a+b
+print(f"total sum: {add(10,20)}")
+
