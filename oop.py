ca9c3bbb20bcd12d38117b0957b6b198a926b3cf
Anu Narayana Naik <anunaik184@gmail.com>
2026-03-15 10:34:58 +0530

    Add classes for Movie, Mobile, Student, and Employee

---
+++ oop.py
@@ -0,0 +1,46 @@
+class Movie:
+    def __init__(self,title,rating):
+        self.title=title
+        self.rating=rating
+    def display(self):
+        print(f"Title:{self.title},Rating:{self.rating}")
+
+m1=Movie("Kantara 1",10)
+m2=Movie("Kantara 2",9)
+m1.display()
+m2.display()
+
+class Mobile:
+    def __init__(self,brand,prize):
+        self.brand=brand
+        self.prize=prize
+    def display(self):
+        print(f"Brand:{self.brand},Prize:{self.prize}")
+m1=Mobile("Vivo",30000)
+m2=Mobile("Realme",50000)
+m1.display()
+m2.display()
+
+
+class Student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def display_info(self):
+        print(f"Name:{self.name},Marks:{self.marks}")    
+s1=Student("Ansha",88)
+s2=Student("Ganesh",91)
+s1.display_info()
+s2.display_info()
+
+### Using Default Parameters
+class Employee:
+    def __init__(self,name,salary=30000):
+        self.name=name
+        self.salary=salary
+    def display(self):
+        print(f"Name:{self.name},Salary:{self.salary}")
+e1=Employee("Anu")
+e2=Employee("Anusha",50000)
+e1.display()
+e2.display()
